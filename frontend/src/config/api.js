3d77839d2e8c0bc979eb5e93d23ebd08810afcd1
yajasvikhanna <yajasvikhanna@gmail.com>
2025-06-23 14:05:09 +0530

    configure backend to use local backend port in dev and deployed port when in prod

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,10 +1,29 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000'
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000'
+
+// export const API_ENDPOINTS = {
+//   QUESTION: `${API_BASE_URL}question/`,
+//   CHECK_ANSWER: `${API_BASE_URL}check-answer/`,
+//   REFRESH_COUNTRIES: `${API_BASE_URL}refresh-countries/`
+// }
+
+
+// export default API_BASE_URL
+
+
+let API_BASE_URL = '';
+
+if (window.location.hostname.includes('localhost') || window.location.hostname === '127.0.0.1') {
+  // Local environment
+  API_BASE_URL = 'http://localhost:8000/api/';
+} else {
+  // Production environment use value from .env
+  API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+}
 
 export const API_ENDPOINTS = {
   QUESTION: `${API_BASE_URL}question/`,
   CHECK_ANSWER: `${API_BASE_URL}check-answer/`,
-  REFRESH_COUNTRIES: `${API_BASE_URL}refresh-countries/`
-}
+  REFRESH_COUNTRIES: `${API_BASE_URL}refresh-countries/`,
+};
 
-
-export default API_BASE_URL
+export default API_BASE_URL;
